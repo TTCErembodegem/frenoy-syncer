9f006baae8f0b2a9e7035b253d908dcadd12a275
Laoujin <woutervs@hotmail.com>
2015-09-10 22:36:27 +0200

    EF6

--- SiteTTCErembodegem/src/Ttc.DataAccess/TtcDbContext.cs
+++ SiteTTCErembodegem/src/Ttc.DataAccess/TtcDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ttc.Model;
+//using Ttc.Model;
 
 namespace Ttc.DataAccess
 {
@@ -17,11 +19,67 @@ namespace Ttc.DataAccess
         }
 
         public DbSet<Speler> Spelers { get; set; }
-        public DbSet<Match> Matches { get; set; }
+        //public DbSet<Match> Matches { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
+    }
+
+    [Table("speler")]
+    public class Speler
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Naam { get; set; }
+
+        public string LinkKaartVTTL { get; set; }
+
+        public string KlassementVTTL { get; set; }
+
+        public string KlassementSporta { get; set; }
+
+        public string Stijl { get; set; }
+
+        public string BesteSlag { get; set; }
+
+        public int? ComputerNummerVTTL { get; set; }
+
+        public string Adres { get; set; }
+
+        public string Gemeente { get; set; }
+
+        public string GSM { get; set; }
+
+        public string Email { get; set; }
+
+        public string Paswoord { get; set; }
+
+        public int? ClubIdVTTL { get; set; }
+
+        public int? ClubIdSporta { get; set; }
+
+        public string NaamKort { get; set; }
+
+        public int? VolgnummerVTTL { get; set; }
+
+        public int? IndexVTTL { get; set; }
+
+        public int? LidNummerSporta { get; set; }
+
+        public int? VolgnummerSporta { get; set; }
+
+        public int? IndexSporta { get; set; }
+
+        [Column("Gestopt")]
+        public int? JaarGestopt { get; set; }
+
+        public int? Toegang { get; set; }
+
+        public string LinkKaartSporta { get; set; }
+
+        public bool IsGestopt { get { return JaarGestopt != null; } }
     }
 }
--- SiteTTCErembodegem/src/Ttc.DataAccess/TtcDbInitializer.cs
+++ SiteTTCErembodegem/src/Ttc.DataAccess/TtcDbInitializer.cs
@@ -4,7 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ttc.Model;
+//using Ttc.Model;
 
 namespace Ttc.DataAccess
 {
